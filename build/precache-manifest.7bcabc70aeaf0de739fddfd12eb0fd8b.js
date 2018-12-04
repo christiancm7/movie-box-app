13834d65b2d0cf18cb4ac75c94aa47662733631c
Christian Morales <atticuschris@yahoo.com>
2018-12-04 14:09:53 -0600

    added the build

---
+++ build/precache-manifest.7bcabc70aeaf0de739fddfd12eb0fd8b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "723feec8bbe80a2b74e0",
+    "url": "/static/css/main.acef14a5.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "1ba19ba05a119e08290a",
+    "url": "/static/js/1.1ba19ba0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "015abc8ec18339e85bbdada2360a5539",
+    "url": "/static/media/placeholder-img.015abc8e.jpg"
+  },
+  {
+    "revision": "723feec8bbe80a2b74e0",
+    "url": "/static/js/main.723feec8.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "1ba19ba05a119e08290a",
+    "url": "/static/css/1.14a4392c.chunk.css"
+  },
+  {
+    "revision": "95cee8e613575f70ed24a840094a9457",
+    "url": "/index.html"
+  }
+];
